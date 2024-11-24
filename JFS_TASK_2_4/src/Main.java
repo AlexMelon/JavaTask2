68d16c18d24764c254ada7cb6d13307d4b1ad041
AlexMelon <axrajan@gmail.com>
2024-11-24 21:27:35 +0530

    24.11.2024

---
+++ JFS_TASK_2_4/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Alex", 31, 001, 45000.0);
+        System.out.println("Employee Details: " + employee);
+    }
+}
